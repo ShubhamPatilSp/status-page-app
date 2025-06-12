95feb7fe946ccf0fa044b5a7e237e9c35848b33d
ShubhamPatilSp <xshuubhampatil@gmail.com>
2025-06-12 15:32:03 +0530

    feat: Use custom secret variable for Auth0

--- frontend/src/app/api/auth/[...auth0]/route.ts
+++ frontend/src/app/api/auth/[...auth0]/route.ts
@@ -1,5 +1,10 @@
 // src/app/api/auth/[auth0]/route.ts
 import { handleAuth } from '@auth0/nextjs-auth0';
 
-export const GET = handleAuth();
-export const POST = handleAuth();
+export const GET = handleAuth({
+  secret: process.env.APP_SESSION_SECRET
+});
+
+export const POST = handleAuth({
+  secret: process.env.APP_SESSION_SECRET
+});
